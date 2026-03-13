2d09bacd47f6002bbd50cddd9abbffd9d147cd99
fagao <fagao@yikangsi.cn>
2026-03-13 18:28:40 +0800

    add some feat

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
-import './styles.css';
+import './index.css';
 
 const root = document.getElementById('root');
 if (root) {
